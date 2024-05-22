32e12f09e7d34666f8b1e7e53d16a4625bb01039
Strive-Sun <50010647+Strive-Sun@users.noreply.github.com>
2024-05-22 21:33:32 +0800

    使用 crashpad 中的 zlib (#8)

--- src/sentry_transport.c
+++ src/sentry_transport.c
@@ -5,8 +5,13 @@
 #include "sentry_ratelimiter.h"
 #include "sentry_string.h"
 
+// 强制开启 SENTRY_TRANSPORT_COMPRESSION，并使用 crashpad 中的 zlib
+#ifndef SENTRY_TRANSPORT_COMPRESSION
+#    define SENTRY_TRANSPORT_COMPRESSION
+#endif // !SENTRY_TRANSPORT_COMPRESSION
+
 #ifdef SENTRY_TRANSPORT_COMPRESSION
-#    include "zlib.h"
+#    include "external/crashpad/third_party/zlib/zlib/zlib.h"
 #endif
 
 #define ENVELOPE_MIME "application/x-sentry-envelope"
